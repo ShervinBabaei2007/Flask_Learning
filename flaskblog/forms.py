df8130d421679a0a0e053be2f6a5868fbabe23bd
Shervin Babaei <shervinb22@gmail.com>
2026-06-19 14:42:23 -0700

    refactor: Package Strcuturing

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -3,20 +3,20 @@ from wtforms import BooleanField, PasswordField, StringField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 
-class RegisterationForm(FlaskForm):
+class RegistrationForm(FlaskForm):
     username = StringField(
-        "Username", validators=[DataRequired(), Length(min=2, max=16)]
+        "Username", validators=[DataRequired(), Length(min=2, max=20)]
     )
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     confirm_password = PasswordField(
         "Confirm Password", validators=[DataRequired(), EqualTo("password")]
     )
-    submit = SubmitField("Sign up")
+    submit = SubmitField("Sign Up")
 
 
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
-    remember = BooleanField("Remember me")
-    submit = SubmitField("Login up")
+    remember = BooleanField("Remember Me")
+    submit = SubmitField("Login")
