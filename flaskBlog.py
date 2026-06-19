b34588c071b8436a06cf59c2f025e464338dc22a
Shervin Babaei <shervinb22@gmail.com>
2026-06-19 09:57:54 -0700

    initial setup

--- flaskBlog.py
+++ flaskBlog.py
@@ -1,8 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        "author": "Shervin Babaei",
+        "title": "Blog Post 1",
+        "content": "First post content",
+        "date_posted": "June 06/2026",
+    },
+    {
+        "author": "Jane Doe",
+        "title": "Blog Post 2",
+        "content": "Second post content",
+        "date_posted": "June 07/2026",
+    },
+]
+
 
 @app.route("/")
-def hello():
-    return "<h1>Hello World!</h1>"
+@app.route("/home")
+def home():
+    return render_template("home.html", posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
