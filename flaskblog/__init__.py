3643856df1751f128fd9a1a5f499a6d423c52ea4
Shervin Babaei <shervinb22@gmail.com>
2026-06-20 11:04:14 -0700

    update: add comment on sqlalchemy

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy # SQL toolkit/ORM - allows us to interact with relational databases
 
 from flaskblog.config import Config
 
